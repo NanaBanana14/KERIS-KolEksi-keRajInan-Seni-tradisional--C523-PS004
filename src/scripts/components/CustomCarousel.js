2a655c0e1389fd4bd9698aff724af0e9f4271822
NanaBanana14 <nananabananayaya@gmail.com>
2023-11-25 00:04:05 +0700

    update corousel

--- src/scripts/components/CustomCarousel.js
+++ src/scripts/components/CustomCarousel.js
@@ -10,7 +10,7 @@ class CustomCarousel extends LitWithoutShadowDom {
   initCarousel() {
     // Inisialisasi Bootstrap Carousel
     const carousel = new bootstrap.Carousel(this.shadowRoot.querySelector('.carousel'), {
-      interval: 2000, // Ganti dengan interval yang diinginkan (dalam milidetik)
+      interval: 2000,
     });
   }
 
@@ -19,7 +19,7 @@ class CustomCarousel extends LitWithoutShadowDom {
       <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https://i.pinimg.com/564x/1e/fe/79/1efe796fe6fcb23eedb50701bfaa8b11.jpg" class="d-block w-100" alt="Slide 1">
+            <img src="https://i.pinimg.com/564x/1e/fe/79/1efe796fe6fcb23eedb50701bfaa8b11.jpg" class="d-block w-100 rounded" alt="Slide 1">
             <div class="carousel-caption d-none d-md-block">
               <h5>Caption Slide 1</h5>
               <p>Deskripsi slide 1.</p>
@@ -27,7 +27,7 @@ class CustomCarousel extends LitWithoutShadowDom {
             </div>
           </div>
           <div class="carousel-item">
-            <img src="https://i.pinimg.com/564x/32/ea/90/32ea90b53541c1790d6d65380a2788a2.jpg" class="d-block w-100" alt="Slide 2">
+            <img src="https://i.pinimg.com/564x/32/ea/90/32ea90b53541c1790d6d65380a2788a2.jpg" class="d-block w-100 rounded" alt="Slide 2">
             <div class="carousel-caption d-none d-md-block">
               <h5>Caption Slide 2</h5>
               <p>Deskripsi slide 2.</p>
@@ -35,7 +35,7 @@ class CustomCarousel extends LitWithoutShadowDom {
             </div>
           </div>
           <div class="carousel-item">
-          <img src="https://i.pinimg.com/564x/1d/b8/69/1db869d783ef20fbc36deaafefb59874.jpg" class="d-block w-100" alt="Slide 2">
+          <img src="https://i.pinimg.com/564x/1d/b8/69/1db869d783ef20fbc36deaafefb59874.jpg" class="d-block w-100 rounded" alt="Slide 2">
           <div class="carousel-caption d-none d-md-block">
             <h5>Caption Slide 2</h5>
             <p>Deskripsi slide 2.</p>
@@ -44,12 +44,10 @@ class CustomCarousel extends LitWithoutShadowDom {
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+         <h1><i class="bi bi-arrow-left-circle-fill"></i></h1>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <h1><i class="bi bi-arrow-right-circle-fill"></i></h1>
         </button>
       </div>
     `;
