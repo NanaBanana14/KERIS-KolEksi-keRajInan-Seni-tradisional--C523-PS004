4ac12f87858ef0552df90b7f44e22541d63e3d48
KamaniaDM <kamania.dm15@gmail.com>
2023-11-28 00:05:22 +0800

    add loginpage

---
+++ src/scripts/views/pages/auth/login.js
@@ -0,0 +1,15 @@
+// function mendaftar() {
+//   window.location.href = 'register.html';
+// }
+
+// function masuk() {
+//   window.location.href = 'index.html';
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const mendaftarLink = document.getElementById('mendaftarLink');
+//   const masukButton = document.getElementById('masukButton');
+
+//   mendaftarLink.addEventListener('click', mendaftar);
+//   masukButton.addEventListener('click', masuk);
+// });
--- webpack.common.js
+++ webpack.common.js
@@ -72,6 +72,22 @@ module.exports = {
       ...htmlWebpackPluginConfig,
     }),
 
+    // autentication
+
+    new HtmlWebpackPlugin({
+      title: 'Login',
+      filename: 'auth/login.html',
+      template: path.resolve(__dirname, 'src/views/auth/login.html'),
+      ...htmlWebpackPluginConfig,
+    }),
+
+    new HtmlWebpackPlugin({
+      title: 'Register',
+      filename: 'auth/register.html',
+      template: path.resolve(__dirname, 'src/views/auth/register.html'),
+      ...htmlWebpackPluginConfig,
+    }),
+
     new CleanWebpackPlugin(),
 
     new CopyWebpackPlugin({
