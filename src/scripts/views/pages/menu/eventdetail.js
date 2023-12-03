69d73a58ffa80d9e0ff8e3838c72a502e2a9384c
NanaBanana14 <nananabananayaya@gmail.com>
2023-12-04 00:53:51 +0700

    update style modal

--- src/scripts/views/pages/menu/eventdetail.js
+++ src/scripts/views/pages/menu/eventdetail.js
@@ -3,7 +3,7 @@ import { createEventDetailTemplate } from '../../templates/template-creator';
 function hideEventDetailModal() {
   const modalElement = document.getElementById('eventDetailModal');
 
-  // Sembunyikan modal
+  // Hidden modal
   modalElement.style.display = 'none';
   modalElement.classList.remove('show');
 }
@@ -11,21 +11,16 @@ function hideEventDetailModal() {
 function showEventDetailModal(eventDetail) {
   const modalElement = document.getElementById('eventDetailModal');
 
-  // Manually show the modal
+  // Show Modal
   modalElement.style.display = 'block';
   modalElement.classList.add('show');
 
-  // Isi konten modal dengan detail acara
-  modalElement.querySelector('.modal-title').textContent = eventDetail.name;
-
-  // Gunakan template untuk mengisi konten modal
+  // content
   const eventDetailTemplate = createEventDetailTemplate(eventDetail);
   modalElement.querySelector('.modal-body').innerHTML = eventDetailTemplate;
 
-  // Atur fokus ke modal
   modalElement.focus();
 
-  // Tambahkan event listener untuk tombol close modal
   modalElement.querySelector('.close').addEventListener('click', hideEventDetailModal);
 }
 
@@ -39,7 +34,6 @@ async function showEventDetailByUrl(detailUrl) {
       return;
     }
 
-    // Panggil fungsi showEventDetailModal untuk menampilkan detail acara dalam modal
     showEventDetailModal(eventDetail);
   } catch (error) {
     console.error(`Error fetching event detail: ${error.message}`);
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -4,7 +4,7 @@ const createEventTemplate = (event) => `
     <img src="${event['image-url']}" class="card-img-top" alt="${event['name']}">
     <div class="card-body">
       <h5 class="card-title mb-5">${event['name']}</h5>
-      <p class="card-text position-absolute bottom-0 start-0 mb-3 mx-3"><i class="bi bi-calendar-event"></i> ${event['date']}</p>
+      <p class="card-text position-absolute bottom-0 start-0 mb-3 mx-3 bi bi-calendar-event"> ${event['date']}</p>
       <a href="#" class="btn btn-brown-white btn-event-detail position-absolute bottom-0 end-0 mb-3 mx-3" data-toggle="modal" data-detail-url=" ${event['detail-url']}">Read more <i class="bi bi-arrow-right-circle-fill"></i></a>
     </div>
   </div>
@@ -12,14 +12,19 @@ const createEventTemplate = (event) => `
 
 const createEventDetailTemplate = (eventDetail) => `
   <div class="card mb-3 col-md-12">
-    <img src="${eventDetail['image-url']}" class="card-img-top w-50" alt="${eventDetail.name}">
-    <div class="card-body">
-      <h5 class="card-title">${eventDetail.name}</h5>
-      <p class="card-text"><strong>Tanggal:</strong> ${eventDetail.date}</p>
-      <p class="card-text"><strong>Deskripsi:</strong> ${eventDetail.description}</p>
-      <!-- Tambahkan elemen lain sesuai kebutuhan -->
+    <div class="d-flex">
+      <img src="${eventDetail['image-url']}" class="card-img-left w-50" alt="${eventDetail.name}">
+      <div class="card-body ms-3">
+      <h5 class="card-title text-center">${eventDetail.name}</h5>
+        <div class="d-flex justify-content-between mt-3 gap-3">
+          <p class="card-text bi bi-calendar-event p-2"> ${eventDetail.date}</p>
+          <p class="card-text bi bi-geo-alt-fill p-2"> ${eventDetail.location}</p>
+        </div>
+        <p class="card-text">${eventDetail.description}</p>
+      </div>
     </div>
   </div>
+
 `;
 
 const createNewsTemplate = (news) => `
