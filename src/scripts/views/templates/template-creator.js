22bafaf9815adcbb3ee326e0b1ff79f78ccbf597
NanaBanana14 <nananabananayaya@gmail.com>
2023-12-04 00:01:03 +0700

    add size modal

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -5,14 +5,14 @@ const createEventTemplate = (event) => `
     <div class="card-body">
       <h5 class="card-title mb-5">${event['name']}</h5>
       <p class="card-text position-absolute bottom-0 start-0 mb-3 mx-3"><i class="bi bi-calendar-event"></i> ${event['date']}</p>
-      <a href="#" class="btn btn-brown-white btn-event-detail position-absolute bottom-0 end-0 mb-3 mx-3" data-detail-url=" ${event['detail-url']}">Read more <i class="bi bi-arrow-right-circle-fill"></i></a>
+      <a href="#" class="btn btn-brown-white btn-event-detail position-absolute bottom-0 end-0 mb-3 mx-3" data-toggle="modal" data-detail-url=" ${event['detail-url']}">Read more <i class="bi bi-arrow-right-circle-fill"></i></a>
     </div>
   </div>
 `;
 
 const createEventDetailTemplate = (eventDetail) => `
-  <div class="card mb-3 col-md-8">
-    <img src="${eventDetail['image-url']}" class="card-img-top" alt="${eventDetail.name}">
+  <div class="card mb-3 col-md-12">
+    <img src="${eventDetail['image-url']}" class="card-img-top w-50" alt="${eventDetail.name}">
     <div class="card-body">
       <h5 class="card-title">${eventDetail.name}</h5>
       <p class="card-text"><strong>Tanggal:</strong> ${eventDetail.date}</p>
